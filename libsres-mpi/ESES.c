d9abd448c28c0fa82e70574874c77f08e70bba68
JackHolland <sirjackholland@gmail.com>
2013-07-11 17:23:16 -0400

    Debugging MPI bug

--- libsres-mpi/ESES.c
+++ libsres-mpi/ESES.c
@@ -90,9 +90,9 @@ void ESInitial(int *argc, char ***argv,   \
   unsigned int outseed;
   int myid, numprocs;
 
-  printf("hi\n");
-  printf("%d\n", *argc);
-  printf("%s", argv[0]);
+  printf("after:\n");
+  printf("<%d> ", *argc);
+  printf("<%s>\n", argv[0]);
   exit(0);
   MPI_Init(argc, argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
--- source/sres.cpp
+++ source/sres.cpp
@@ -140,6 +140,8 @@ void init_sres (input_params& ip, sres_params& sp) {
 		cout << term->red << "The given number of dimensions does not have ranges programmed in! Please check that the given number (" << dim << ") is correct or add ranges to sres.cpp." << term->reset << endl;
 	}
 	
+	cerr << "before:" << endl;
+	cerr << "<" << ip.argc << "> <" << ip.argv[0] << ">" << endl;
 	ESInitial(
 	#if defined(OPEN_MPI)
 		&(ip.argc), &(ip.argv),

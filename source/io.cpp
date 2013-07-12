dbe6a86895c320a17f586db8b1bd91b84c7db7a1
JackHolland <sirjackholland@gmail.com>
2013-07-12 13:43:07 -0400

    Score test

--- source/io.cpp
+++ source/io.cpp
@@ -77,7 +77,6 @@ double simulate_set (double parameters[]) {
 	}
 	free(sim_args);
 	
-	cout << "score: " << (double)score << " / " << max_score << " (" << parameters[0] << ")" << endl;
 	return 1 - ((double)score / max_score);
 }
 

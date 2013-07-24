83b9836e7a3ee17892f216e543772f4a313662e1
JackHolland <sirjackholland@gmail.com>
2013-07-24 11:09:07 -0400

    Integral value testing

--- source/sres.cpp
+++ source/sres.cpp
@@ -114,6 +114,7 @@ void fitness (double* parameters, double* score, double* constraints) {
 	} else {
 		parameters[2] = 300;
 	}
+	cerr << "params: " << parameters[0] << " " << parameters[1] << " " << parameters[2] << endl;
 	*score = simulate_set(parameters);
 }
 

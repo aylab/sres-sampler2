9026e45338d8865163d22c2a8e62563b3130fd48
JackHolland <sirjackholland@gmail.com>
2013-08-11 11:20:41 -0400

    Removed rank indicator from print good sets message since only the master process prints them

--- source/io.cpp
+++ source/io.cpp
@@ -314,9 +314,7 @@ double simulate_set (double parameters[]) {
 */
 void print_good_set (double parameters[], double score) {
 	if (ip.print_good_sets && score <= ip.good_set_threshold) {
-		cout << "  ";
-		term->rank(get_rank());
-		cout << term->blue << "Found a good set " << term->reset << "(score " << score << ")" << endl;
+		cout << term->blue << "  Found a good set " << term->reset << "(score " << score << ")" << endl;
 		ip.good_sets_stream << parameters[0];
 		for (int i = 1; i < ip.num_dims; i++) {
 			ip.good_sets_stream << "," << parameters[i];

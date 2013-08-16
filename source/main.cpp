3920fe92f801b43858cdbe74cebc135e3396a3f6
JackHolland <sirjackholland@gmail.com>
2013-08-16 13:14:18 -0400

    Removed obsolete term from the usage message

--- source/main.cpp
+++ source/main.cpp
@@ -116,7 +116,7 @@ void usage (const char* message) {
 	cout << "-g, --generations        [int]        : the number of generations to run before returning results, min=1, default=1750" << endl;
 	cout << "-s, --seed               [int]        : the seed used in the evolutionary strategy (not simulations), min=1, default=time" << endl;
 	cout << "-e, --printing-precision [int]        : how many digits of precision parameters should be printed with, min=1, default=6" << endl;
-	cout << "-a, --arguments          [N/A]        : every argument following this will be sent to the deterministic simulation" << endl;
+	cout << "-a, --arguments          [N/A]        : every argument following this will be sent to the simulation" << endl;
 	cout << "-c, --no-color           [N/A]        : disable coloring the terminal output, default=unused" << endl;
 	cout << "-v, --verbose            [N/A]        : print detailed messages about the program state" << endl;
 	cout << "-q, --quiet              [N/A]        : hide the terminal output, default=unused" << endl;

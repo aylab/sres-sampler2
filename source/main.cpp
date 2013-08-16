b21060d400009c4bace58389e2cd4bbbd3fa7d1d
JackHolland <sirjackholland@gmail.com>
2013-08-16 10:52:56 -0400

    Fixed default argument values

--- source/main.cpp
+++ source/main.cpp
@@ -114,7 +114,7 @@ void usage (const char* message) {
 	cout << "-G, --good-set-threshold [float]      : the worst score a set must receive to be printed to the good sets file, default=0.0" << endl;
 	cout << "-d, --dimensions         [int]        : the number of dimensions (i.e. rate parameters) to explore, min=1, default=3" << endl;
 	cout << "-P, --parent-population  [int]        : the population of parent simulations to use each generation, min=1, default=3" << endl;
-	cout << "-p, --total-population   [int]        : the population of child simulations to use each generation, min=1, default=20" << endl;
+	cout << "-p, --total-population   [int]        : the population of total simulations to use each generation, min=1, default=20" << endl;
 	cout << "-g, --generations        [int]        : the number of generations to run before returning results, min=1, default=1750" << endl;
 	cout << "-s, --seed               [int]        : the seed used in the evolutionary strategy (not simulations), min=1, default=time" << endl;
 	cout << "-e, --printing-precision [int]        : how many digits of precision parameters should be printed with, min=1, default=6" << endl;
